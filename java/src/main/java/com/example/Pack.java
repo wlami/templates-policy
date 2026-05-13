b7f838189e01ed8d8c0a4bb272f9cda9c276710a
Wladi Mitzel <wladi@pulumi.com>
2026-05-13 09:00:36 +0200

    java template: target aws:s3/bucketAclV2:BucketAclV2 instead of deprecated bucket.acl
    The legacy 'acl' property on aws:s3/bucket:Bucket was deprecated in
    the AWS provider; ACLs moved to a separate aws:s3/bucketAclV2:
    BucketAclV2 resource. Update the scaffolded policy to flag the new
    resource type (and the older aws:s3/bucketAcl:BucketAcl name for
    provider versions in between).

--- java/src/main/java/com/example/Pack.java
+++ java/src/main/java/com/example/Pack.java
@@ -12,14 +12,18 @@ public class Pack {
             .enforcementLevel(EnforcementLevel.ADVISORY)
             .policies(ResourceValidationPolicy.builder()
                 .name("s3-no-public-read")
-                .description("Prohibits setting the publicRead or publicReadWrite permission on AWS S3 buckets.")
+                .description("Prohibits setting the publicRead or publicReadWrite permission on AWS S3 bucket ACLs.")
                 .enforcementLevel(EnforcementLevel.MANDATORY)
                 .validate((rArgs, report) -> {
-                  if ("aws:s3/bucket:Bucket".equals(rArgs.type())) {
+                  // The legacy `acl` property on aws:s3/bucket:Bucket is deprecated;
+                  // ACLs now live on aws:s3/bucketAclV2:BucketAclV2 (and on
+                  // aws:s3/bucketAcl:BucketAcl in older provider versions). Flag both.
+                  if ("aws:s3/bucketAclV2:BucketAclV2".equals(rArgs.type())
+                      || "aws:s3/bucketAcl:BucketAcl".equals(rArgs.type())) {
                     Object acl = rArgs.props().get("acl");
                     if ("public-read".equals(acl) || "public-read-write".equals(acl)) {
                       report.violation(
-                          "You cannot set public-read or public-read-write on an S3 bucket. "
+                          "Bucket ACL must not be public-read or public-read-write. "
                           + "Read more about ACLs here: "
                           + "https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html");
                     }
